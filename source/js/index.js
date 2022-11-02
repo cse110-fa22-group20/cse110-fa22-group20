c037a6cb8ef1a53acae90c52fda4ea22bda3ddc8
Navid Boloorian <navidboloorian0@gmail.com>
2022-11-01 20:17:19 -0700

    added boilerplate

---
+++ source/js/index.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', init);
+
+// ensures that page as loaded before running anything
+function init() {
+    console.log("successfully loaded!"); // remove
+}
