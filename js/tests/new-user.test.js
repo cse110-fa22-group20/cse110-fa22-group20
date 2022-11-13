9c3b2974ab1304681e462e58e4f73974b145c6df
Roalnd Wen <hwen@ucsd.edu>
2022-11-12 20:41:07 -0800

    fixed tests

--- js/tests/new-user.test.js
+++ js/tests/new-user.test.js
@@ -1,4 +1,8 @@
 import { checkUserExist, checkProfile, updateImage, getFormData } from './new-user.js';
+const checkUserExist = require("../new-user.js").checkUserExist;
+const checkProfile = require("../new-user.js").checkProfile;
+const updateImage = require("../new-user.js").updateImage;
+const getFormData = require("../new-user.js").getFormData;
 
 // just a dummy test
 test("Dummy test", () => {
