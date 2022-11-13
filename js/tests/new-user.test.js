25a80faeac4f4ed9cf28ba5216a232f90aac5cba
Roalnd Wen <hwen@ucsd.edu>
2022-11-12 20:35:07 -0800

    Fixed html validation errors
    Co-authored-by: Mariel Chua <marielchua@users.noreply.github.com>

--- js/tests/new-user.test.js
+++ js/tests/new-user.test.js
@@ -1,4 +1,4 @@
-import { checkUserExist, checkProfile, getDetails, parseFormData } from './db.js';
+import { checkUserExist, checkProfile, updateImage, getFormData } from './new-user.js';
 
 // just a dummy test
 test("Dummy test", () => {
