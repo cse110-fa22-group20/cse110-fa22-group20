a9d28dbc2b8d316be641244b2222f518617d1db2
Roalnd Wen <hwen@ucsd.edu>
2022-11-14 09:19:02 -0800

    updated dummy test, only checkProfile is tested

--- js/tests/new-user.test.js
+++ js/tests/new-user.test.js
@@ -1,12 +1,15 @@
 /**
  * @jest-environment jsdom
  */
-const checkUserExist = require("../new-user.js").checkUserExist;
+// const checkUserExist = require("../new-user.js").checkUserExist;
 const checkProfile = require("../new-user.js").checkProfile;
-const updateImage = require("../new-user.js").updateImage;
-const getFormData = require("../new-user.js").getFormData;
+// const updateImage = require("../new-user.js").updateImage;
+// const getFormData = require("../new-user.js").getFormData;
 
 // just a dummy test
 test("Dummy test", () => {
-    expect(checkUserExist()).toBe(true);
+    const profileObj = {
+        name: "A"
+    };
+    expect(checkProfile(profileObj)).toBe(false);
 });
