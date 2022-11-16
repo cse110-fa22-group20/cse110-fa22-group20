56dcfb2648c04ffaec98da66091880dd5b543dc7
Roalnd Wen <hwen@ucsd.edu>
2022-11-15 16:54:11 -0800

    added checkProfile tests

--- js/tests/new-user.test.js
+++ js/tests/new-user.test.js
@@ -4,7 +4,41 @@
 require("fake-indexeddb/auto");
 const newUser = require("../new-user.js");
 
-// just a dummy test
-test("Dummy test", () => {
-    expect(newUser.checkProfile(null)).toBe(false);
+describe("checkProfile tests", () => {
+    test("valid profileObj", () => {
+        const profileObj = {
+            name: "name",
+            image: "image",
+            description: "test",
+            primaryColor: null,
+            secondaryColor: null
+        };
+        expect(newUser.checkProfile(profileObj)).toBe(true);
+    });
+    test("profileObj without a name is invalid", () => {
+        const profileObj = {
+            image: "image",
+            description: "test",
+            primaryColor: null,
+            secondaryColor: null
+        };
+        expect(newUser.checkProfile(undefined)).toBe(false);
+    });
+    test("profileObj without a name length of 9 is invalid", () => {
+        const profileObj = {
+            name: "",
+            image: "image",
+            description: "test",
+            primaryColor: null,
+            secondaryColor: null
+        };
+        expect(newUser.checkProfile(undefined)).toBe(false);
+    });
+    test("null is invalid", () => {
+        expect(newUser.checkProfile(null)).toBe(false);
+    });
+    test("undefined is invalid", () => {
+        expect(newUser.checkProfile(undefined)).toBe(false);
+    });
 });
+
