5f1191930c381db746dc8850365b6302521ab28c
Roalnd Wen <hwen@ucsd.edu>
2022-11-15 17:16:12 -0800

    added db-for-test.js so db funcs work inside tests

--- js/tests/new-user.test.js
+++ js/tests/new-user.test.js
@@ -3,7 +3,7 @@
  */
 require("fake-indexeddb/auto");
 const newUser = require("../new-user.js");
-const db = require("../db.js");
+const db = require("db-for-test.js");
 
 describe("checkProfile tests", () => {
     test("valid profileObj", () => {
