e33e166c206bafd633bb158a91b2a3eafc0a6373
Roalnd Wen <hwen@ucsd.edu>
2022-11-22 20:32:50 -0800

    made the tests work

--- source/js/tests/main.test.js
+++ source/js/tests/main.test.js
@@ -5,8 +5,170 @@ require("fake-indexeddb/auto");
 const main =  require('../main');
 const db = require('../db');
 
-test("'populatePosts' zero posts.", () => {
-    expect(1).toBe(1);
+describe("populatePosts tests", () => {
+    test("zero posts.", () => {
+        const state = {
+            postIDCounter: 0,
+            posts: [],
+        };
+        const postContainer = document.createElement('section');
+        postContainer.setAttribute('id', '#posts-wrapper');
+        const typeSelector = document.createElement('div');
+        typeSelector.setAttribute('id', 'post-type-selector');
+        typeSelector.setAttribute('class', 'hidden');
+        postContainer.appendChild(typeSelector);
+        document.body.innerHTML = '<div id="root"></div>'
+        document.querySelector('#root').appendChild(postContainer);
+        main.populatePosts(state.posts);
+        expect(postContainer.childElementCount).toBe(1);
+    });
+
+    test("five posts.", () => {
+        const state = {
+            postIDCounter: 0,
+            posts: [],
+        };
+    
+        for (let i = 0; i < 5; i++) {
+            const newPost = {
+                id: i,
+                type: 'text',
+                content: 'dummy text',
+            };
+            state.posts.push(newPost);
+            state.postIDCounter++;
+        }
+    
+        const postContainer = document.createElement('section');
+        postContainer.setAttribute('id', 'posts-wrapper');
+        const typeSelector = document.createElement('div');
+        typeSelector.setAttribute('id', 'post-type-selector');
+        typeSelector.setAttribute('class', 'hidden');
+        postContainer.appendChild(typeSelector);
+        document.body.innerHTML = '<div id="root"></div>'
+        document.querySelector('#root').appendChild(postContainer);
+    
+        main.populatePosts(state.posts);
+        expect(postContainer.childElementCount).toBe(6);
+    });
+});
+
+
+test("Test 'createTextPostObject'", () => {
+    const newPost = {
+        id: 20,
+        type: 'text',
+        content: 'dummy text',
+    };
+
+    const post = main.createTextPostObject(newPost);
+    expect(post.tagName).toBe('DIV');
+    expect(post.getAttribute('data-post-id')).toBe('20');
+    expect(post.getAttribute('class')).toBe('post text-post');
+    expect(post.querySelector('pre').innerText).toBe('dummy text');
+});
+
+describe("appendPost tests", () => {
+    test("one time.", () => {
+        const newPost = {
+            id: 0,
+            type: 'text',
+            content: 'dummy text',
+        };
+    
+        const postContainer = document.createElement('section');
+        postContainer.setAttribute('id', 'posts-wrapper');
+        const typeSelector = document.createElement('div');
+        typeSelector.setAttribute('id', 'post-type-selector');
+        typeSelector.setAttribute('class', 'hidden');
+        postContainer.appendChild(typeSelector);
+        document.body.innerHTML = '<div id="root"></div>'
+        document.querySelector('#root').appendChild(postContainer);
+    
+        main.appendPost(newPost);
+    
+        const el = document.querySelector('#root').firstChild.firstChild;
+        expect(el.tagName).toBe('DIV');
+        expect(el.getAttribute('data-post-id')).toBe('0');
+        expect(el.getAttribute('class')).toBe('post text-post');
+        expect(el.querySelector('pre').innerText).toBe('dummy text');
+    });
+    
+    test("'appendPost' two times.", () => {
+        const newPost1 = {
+            id: 0,
+            type: 'text',
+            content: 'dummy text',
+        };
+    
+        const newPost2 = {
+            id: 1,
+            type: 'text',
+            content: 'dummy text',
+        };
+    
+        const postContainer = document.createElement('section');
+        postContainer.setAttribute('id', 'posts-wrapper');
+        const typeSelector = document.createElement('div');
+        typeSelector.setAttribute('id', 'post-type-selector');
+        typeSelector.setAttribute('class', 'hidden');
+        postContainer.appendChild(typeSelector);
+        document.body.innerHTML = '<div id="root"></div>'
+        document.querySelector('#root').appendChild(postContainer);
+    
+        main.appendPost(newPost1);
+        main.appendPost(newPost2);
+    
+        const el1 = document.querySelector('#root').firstChild.firstChild;
+        expect(el1.tagName).toBe('DIV');
+        expect(el1.getAttribute('data-post-id')).toBe('0');
+        expect(el1.getAttribute('class')).toBe('post text-post');
+        expect(el1.querySelector('pre').innerText).toBe('dummy text');
+    
+        const el2 = document.querySelector('#root').firstChild.firstChild.nextSibling;
+        expect(el2.tagName).toBe('DIV');
+        expect(el2.getAttribute('data-post-id')).toBe('1');
+        expect(el2.getAttribute('class')).toBe('post text-post');
+        expect(el2.querySelector('pre').innerText).toBe('dummy text');
+    });
+});
+
+test("'insertPost'", () => {
+    const newPost1 = {
+        id: 0,
+        type: 'text',
+        content: 'dummy text',
+    };
+
+    const newPost2 = {
+        id: 1,
+        type: 'text',
+        content: 'dummy text',
+    };
+
+    const postContainer = document.createElement('section');
+    postContainer.setAttribute('id', 'posts-wrapper');
+    const typeSelector = document.createElement('div');
+    typeSelector.setAttribute('id', 'post-type-selector');
+    typeSelector.setAttribute('class', 'hidden');
+    postContainer.appendChild(typeSelector);
+    document.body.innerHTML = '<div id="root"></div>'
+    document.querySelector('#root').appendChild(postContainer);
+
+    main.appendPost(newPost1);
+    main.insertPost(newPost2, 0);
+
+    const el1 = document.querySelector('#root').firstChild.firstChild;
+    expect(el1.tagName).toBe('DIV');
+    expect(el1.getAttribute('data-post-id')).toBe('1');
+    expect(el1.getAttribute('class')).toBe('post text-post');
+    expect(el1.querySelector('pre').innerText).toBe('dummy text');
+
+    const el2 = document.querySelector('#root').firstChild.firstChild.nextSibling;
+    expect(el2.tagName).toBe('DIV');
+    expect(el2.getAttribute('data-post-id')).toBe('0');
+    expect(el2.getAttribute('class')).toBe('post text-post');
+    expect(el2.querySelector('pre').innerText).toBe('dummy text');
 });
 
 describe("deletePost tests", () => {
