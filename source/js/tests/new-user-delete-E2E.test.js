525e5a822773be3a02362736bacd4a4b49d2d59e
Roalnd Wen <hwen@ucsd.edu>
2022-11-25 12:35:31 -0800

    added 2 more tests

--- source/js/tests/new-user-delete-E2E.test.js
+++ source/js/tests/new-user-delete-E2E.test.js
@@ -1,6 +1,14 @@
+const puppeteer = require('puppeteer');
+var browers, page;
 describe('New User', () => {
     beforeAll(async () => {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
         await page.goto('http://127.0.0.1:5500/source/pages/new-user.html');
+    });
+
+    afterAll(async () => {
+        await browser.close();
     });
   
     it('Check the fields are there', async () => {
@@ -13,6 +21,43 @@ describe('New User', () => {
         const submit = await page.$('#go-button');
         await submit.click();
         // blank form is not permitted, no redirection
-        expect(page.url()).toBe('http://127.0.0.1:5500/source/pages/new-user.html');
+        expect(await page.url()).toBe('http://127.0.0.1:5500/source/pages/new-user.html');
     }, 1000);
+
+    it('Submitting a form without user name', async () => {
+        const description = await page.$('#user-description');
+        description.innerText = "A";
+        const imageHandler = await page.$('#user-image');
+        await imageHandler.uploadFile('./../../assets/placeholder-profile-pic.png');
+        const submit = await page.$('#go-button');
+        await submit.click();
+        // no name is not permitted, no redirection
+        expect(await page.url()).toBe('http://127.0.0.1:5500/source/pages/new-user.html');
+    }, 1000);
+
+    it('Submitting a form without profile picture', async () => {
+        const name = await page.$('#user-name');
+        name.innerText = "A";
+        const description = await page.$('#user-description');
+        description.innerText = "A";
+        const submit = await page.$('#go-button');
+        await submit.click();
+        // no name is not permitted, no redirection
+        expect(await page.url()).toBe('http://127.0.0.1:5500/source/pages/new-user.html');
+    }, 1000);
+
+    /*it('Submitting a form with everything', async () => {
+        const name = await page.$('#user-name');
+        name.innerText = "A";
+        const description = await page.$('#user-description');
+        description.innerText = "A";
+        const imageHandler = await page.$('#user-image');
+        await imageHandler.uploadFile('./../../assets/placeholder-profile-pic.png');
+        const [response] = await Promise.all([
+            page.waitForNavigation(), // The promise resolves after navigation has finished
+            page.click('#go-button'), // Clicking the link will indirectly cause a navigation
+        ]); 
+        console.log(respose);
+        expect(await page.url()).toBe('http://127.0.0.1:5500/source/pages/main.html');
+    }, 10000);*/
 });
