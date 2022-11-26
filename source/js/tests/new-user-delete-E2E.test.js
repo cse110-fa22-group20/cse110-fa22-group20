ac5c8e0d48640166812ed7f0a440fdfa2c248a8a
Roalnd Wen <hwen@ucsd.edu>
2022-11-26 10:52:59 -0800

    deleted a test

--- source/js/tests/new-user-delete-E2E.test.js
+++ source/js/tests/new-user-delete-E2E.test.js
@@ -60,8 +60,8 @@ describe('New User', () => {
         expect(page.url()).toBe(mainUrl);
     });
 
-    it('Should be redirected to main instantly', async () => {
+    /*it('Should be redirected to main instantly', async () => {
         page.goto(newUserUrl);
         expect(page.url()).toBe(mainUrl);
-    });
+    });*/
 });
